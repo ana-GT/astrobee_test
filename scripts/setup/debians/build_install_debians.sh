25a6644467de50057e01d55438a3b3df55aa3b0e
Ana C. Huaman Quispe <ana@traclabs.com>
2023-10-19 10:21:32 -0500

    Using old opencv

--- scripts/setup/debians/build_install_debians.sh
+++ scripts/setup/debians/build_install_debians.sh
@@ -52,7 +52,7 @@ case $dist in
 esac
 
 # Add public debians to build list
-build_list+=( dlib dbow2 gtsam decomputil jps3d openmvg ) # alvar
+build_list+=( alvar dlib dbow2 gtsam decomputil jps3d openmvg )
 # If restricted rti-dev debian is present, add miro and soracore as well
 dpkg-query -W -f='${Status}\n' rti-dev 2>&1 | grep -q "install ok installed" &&
 echo "Package rti-dev exists. Including miro and soracore to build list..." &&
